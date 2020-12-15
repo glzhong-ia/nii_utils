0ed8ca63bac312de12660f5ea7f19bae740e5171
zhonggangliang <zhonggangliang@hotmail.com>
2020-12-15 09:41:45 +0800

    ADD: drawROI

--- drawROI.py
+++ drawROI.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-\
 
 import nibabel as nib
-import numpy.matlib
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-path = 'E:/SynologyDrive/临时资料/手指fmri/MNI152_T1_1mm_brain.nii'
-data = nib.load(path)
+mask_path = './commonMask.hdr'
+spm_path = './spmT_0001.nii'
+mask_nii = nib.load(mask_path)
+spm_nii = nib.load(spm_path)
 
-
+spm_dot_mask = spm_nii.get_data() * mask_nii.get_data()
+max_pos = np.unravel_index(np.argmax(spm_dot_mask), spm_dot_mask.shape)
+# max_pos_axis = np.array(max_pos) + np.array(common_region['t'])
 print('aaa')
